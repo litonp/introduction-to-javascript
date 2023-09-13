904b37333baf32b9d5406cae556cd3831938bb07
litonp <litondas7121@gmail.com>
2023-09-13 22:38:55 +0600

    apple

---
+++ operation.js
@@ -0,0 +1,18 @@
+var onionPrice = 70;
+var eggPrice = 13;
+
+// var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+// var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+
+// var orangePrice = 20;
+// var quantiy = 7;
+// var totalPrice = orangePrice * quantiy;
+// console.log(totalPrice);
+
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
---
+++ variable-name.js
@@ -0,0 +1,19 @@
+var price = 29;
+
+// 1. variable name can not be any keywords
+var false = 96;
+var return = true;
+
+// 2. variable name has to be in one work. No space
+var my home address = "new alifornia";
+
+// 3. variable name can not have quatation
+var "name" = "tom";
+
+// 4.variable name can not starts with a number but can ends with a number
+var 99club = 1994;
+var club25 = 2023;
+
+// 5. how to use lobg names
+var user_name = "bappa raj";
+var userName = "bappa raj";      we will use this one
