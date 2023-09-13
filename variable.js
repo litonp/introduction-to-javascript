21bd8060d9db593b4fa27db55d4de5bc730971ab
litonp <litondas7121@gmail.com>
2023-09-13 22:12:36 +0600

    variable

--- variable.js
+++ variable.js
@@ -1,6 +1,18 @@
 // vary + able = variable 
+
+// numeric
 var price = 13;
 var age = 23;
 var temperature = 38;
 var weight = 75;
 var money = 580;
+
+// string 
+var person = "liton das";
+var location = "dhaka mirpur";
+var special = 'puja';
+
+// Boolean
+var serious = true;
+var isFullmarks = true;
+var isSingle = false;
