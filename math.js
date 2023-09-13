a014d969698a3bb9255d11d0248f380be99dd9ac
litonp <litondas7121@gmail.com>
2023-09-13 21:19:33 +0600

    main

---
+++ math.js
@@ -0,0 +1,18 @@
+/***
+ * 1. Number ==> 55, 898, -32
+ * 2. Integer, odd, even: 2, 5,
+ * 3. float: decimal: 2.5, 13.67, 32.69,
+ * 4. oparation addition, substraction, multiplication, division, remainder
+ * 5. percent, 
+ * 6. average 
+ * 7. compaison: >, <, ==,
+ * 8. oikik: 2 kilo apple and 5 kilo angur :
+ * 9. 45, 25, 68, 68, 129
+ * 10. series
+ * :::EXTRA:::
+ * 1. Prime
+ * 2. premutation
+ * 3. area: 
+ * :::ALWAYS:::
+ * jodi lage
+ * ***/
---
+++ variable.js
@@ -0,0 +1,6 @@
+// vary + able = variable 
+var price = 13;
+var age = 23;
+var temperature = 38;
+var weight = 75;
+var money = 580;
