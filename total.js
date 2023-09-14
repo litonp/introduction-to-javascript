ccc5b339c6de495fed26c5b2675693097d1c529a
litonp <litondas7121@gmail.com>
2023-09-14 21:54:11 +0600

    complete

---
+++ total.js
@@ -0,0 +1,28 @@
+var money = 1000;
+var orange = 400;
+var apple = 300;
+var totalPrice = money - orange - apple;
+console.log(totalPrice);
+
+var num1 = 75.25;
+var num2 = 65;
+var num3 = 80;
+var num4 = 35.45;
+var num5 = 99.50;
+var num6 = 5;
+var totalNum = num1 + num2 + num3 + num4 + num5;
+totalNum /= 5;
+totalNum = totalNum.toFixed(2);
+totalNum = parseFloat(totalNum);
+console.log(totalNum);
+
+var firstName = "I am going to be";
+var lastName = " an awesome web developer";
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+var money = 119;
+var person = 5;
+var remainder = money % person;
+console.log(remainder);
+
